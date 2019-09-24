b3790b65a8713f5f18420aa361dfa294e2ec53df
Hein Htet <heinhtet@codigo.co>
2019-09-24 13:23:51 +0630

    remove api key for static

--- sample-rx/src/main/java/com/deevvdd/sample_rx/data/network/URL.kt
+++ sample-rx/src/main/java/com/deevvdd/sample_rx/data/network/URL.kt
@@ -1,6 +1,7 @@
 package com.deevvdd.sample_rx.data.network
 
-const val API_KEY = "8626277-c3018f509c3bbe18b552b6863"
+import com.deevvdd.sample_rx.BuildConfig
+
+
 const val BASE_URL = "https://pixabay.com/api/"
-const val KEY = "?key=$API_KEY"
-const val LATEST = "https://pixabay.com/api/order?=latest"
+const val KEY = "?key=${BuildConfig.API_KEY}"
